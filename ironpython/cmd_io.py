a2c5df3c8521c2719d7eefcfb10dccd8787cc7b9
chunlee888 <chunlee@gmail.com>
2015-11-12 13:19:58 -0800

    Update cmd_io.py

--- ironpython/cmd_io.py
+++ ironpython/cmd_io.py
@@ -1,8 +1,25 @@
-refactor the e7 __init__.py to improve 
+"""
+sample code to control "cmd" i/o programetically
+"""
 
-seperate the dhcp setup and control code
+def tostring(buf, max):
+    _s = []
+    for i in range(0, max):
+        _s.append(str(buf[i]))
+    return "".join(_s)
 
-define helper dhcp function(s)
+from System.Diagnostics import Process
+p = Process()
+p.StartInfo.UseShellExecute = False
+p.StartInfo.RedirectStandardOutput = True
+p.StartInfo.RedirectStandardInput = True
+p.StartInfo.FileName = 'cmd.exe'
+p.StartInfo.Arguments = '/k'
 
-session debug message
+p.Start()
+i = p.StandardInput
+o = p.StandardOutput
 
+from System import Array, Char
+buf = Array[Char](range(128))
+read = o.ReadAsync(buf, 0, 128)
